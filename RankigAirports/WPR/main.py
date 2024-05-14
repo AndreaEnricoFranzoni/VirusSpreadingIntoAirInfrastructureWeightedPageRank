49dc6c9e5cbc8d13dd23c57b800298803206060d
Lennard Scheibel <contact@lennardscheibel.de>
2024-05-14 19:04:49 +0200

    Plot airports on interactive map

--- RankigAirports/WPR/main.py
+++ RankigAirports/WPR/main.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
+import plotly.express as px
+
+from DEG import DEG
 from WPR import WPR
-from PR import PR
+
+airports_data = pd.read_csv('airports.csv')
 
 A = pd.read_csv('A.csv', index_col=0)  # Adjacency matrix
 W = pd.read_csv('W.csv', index_col=0)  # Weights matrix
-
 
 
 
@@ -16,23 +19,48 @@ initially_infected_at_airports = np.ones(A.shape[0], dtype=float)
 
 wpr = WPR(A.to_numpy(), W.to_numpy(), 0.95, 0.9, 0.45, initially_infected_at_airports)
 
-
-
 # Rank airports.
-#WPR
-#wpr.converge(1, 25)
-#PR
+# WPR
+# wpr.converge(1, 25)
+# PR
 
 
-#ranks_wpr = wpr.ranks
+# ranks_wpr = wpr.ranks
 
-
-pr = PR(A.to_numpy(),0.95)
-pr.converge(1,2)
-ranks_pr = pr.ranks
 airports = A.keys().tolist()
-idx_sort = ranks_pr.argsort()
+
+# pr = PR(A.to_numpy(), 0.95)
+# pr.converge(1, 2)
+# ranks_pr = pr.ranks
+# idx_sort_pr = ranks_pr.argsort()
+
+deg = DEG(A.to_numpy())
+ranks_deg = np.array(deg.degree_in)
+idx_sort_deg = ranks_deg.argsort()
+
+
 # idx_sort = range(len(ranks)) # Sorts alphabetically.
 
-for airport_idx in idx_sort:
-    print("{airport}: {value}".format(airport=airports[airport_idx], value=ranks_pr[airport_idx]))
+for airport_idx in idx_sort_deg:
+    print("{airport}: {value}".format(airport=airports[airport_idx], value=ranks_deg[airport_idx]))
+
+def draw_map():
+    color_scale = [(0, 'orange'), (1, 'red')]
+    fig = px.scatter_mapbox(
+        airports_data,
+        lat="Latitude",
+        lon="Longitude",
+        hover_name="Name",
+        hover_data=["IATA", "ICAO"],
+        # color="Listed",
+        # color_continuous_scale=color_scale,
+        # size="Listed",
+        zoom=8,
+        height=800,
+        width=1600
+    )
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.show()
+
+# draw_map()
