a0da186d10253ed3c56d43c1ee91e311c06df961
Lennard Scheibel <contact@lennardscheibel.de>
2024-05-15 14:45:43 +0200

    Add new results

--- RankigAirports/WPR/main.py
+++ RankigAirports/WPR/main.py
@@ -16,7 +16,7 @@ alpha = 0.45    #tradeoff between strength and infection
 
 #FIRST CASE
 #only 2% of people in Stockholm ARL is infected
-name_airports_with_infected = ["FRA", "CDG", "AMS", "IST", "ATL", "PEK", "ORD", "MUC", "DME", "DXB"]
+name_airports_with_infected = ["FNA", "CKY", "ROB"]
 
 #SECOND CASE
 #name_airports_with_infected = ("FRA", "CDG")
@@ -73,7 +73,7 @@ for p_alpha in p_alphas:
         new_line = pd.DataFrame({'Airport': [airports[airports_idx]], 'Ranking': [ranks[airports_idx]]})
         results = pd.concat([results, new_line], ignore_index=True)
 
-    results.to_csv(f"/Users/lscheibel/Projects/DataMiningProject/RankingResults/WPR/without_recovery/BigAirports/results_WPR_big_airports_{p_alpha}.csv")
+    results.to_csv(f"/Users/lscheibel/Projects/DataMiningProject/RankingResults/WPR/without_recovery/Ebola/results_WPR_big_airports_{p_alpha}.csv")
 
     #printing the results
     count = 0
